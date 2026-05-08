db024400aaefc9d517075fba309151c2d9d8c284
afernandezde <116562097+afernandezde@users.noreply.github.com>
2026-05-08 18:38:27 +0200

    Fix: Resuelto ticket KAN-3

--- src/main/java/com/bezkoder/spring/hibernate/onetomany/model/Tutorial.java
+++ src/main/java/com/bezkoder/spring/hibernate/onetomany/model/Tutorial.java
@@ -19,14 +19,18 @@ public class Tutorial {
     @Column(name = "published")
     private boolean published;
 
+    @Column(name = "author") // Agregado para almacenar el autor del tutorial
+    private String author; // Agregado para almacenar el autor del tutorial
+
     public Tutorial() {
 
     }
 
-    public Tutorial(String title, String description, boolean published) {
+    public Tutorial(String title, String description, boolean published, String author) {
         this.title = title;
         this.description = description;
         this.published = published;
+        this.author = author; // Agregado para inicializar el atributo "author"
     }
 
     public long getId() {
@@ -57,8 +61,16 @@ public class Tutorial {
         this.published = isPublished;
     }
 
+    public String getAuthor() { // Agregado para acceder al atributo "author"
+        return author;
+    }
+
+    public void setAuthor(String author) { // Agregado para modificar el atributo "author"
+        this.author = author;
+    }
+
     @Override
     public String toString() {
-        return "Tutorial [id=" + id + ", title=" + title + ", description=" + description + ", published=" + published + "]";
+        return "Tutorial [id=" + id + ", title=" + title + ", description=" + description + ", published=" + published + ", author=" + author + "]";
     }
 }
