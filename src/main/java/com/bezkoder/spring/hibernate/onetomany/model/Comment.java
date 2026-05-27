81b0ec6a0605b735b83891d1ea9486cead8df4ea
afernandezde <116562097+afernandezde@users.noreply.github.com>
2026-05-27 12:30:41 +0200

    Fix: Resuelto ticket KAN-4

--- src/main/java/com/bezkoder/spring/hibernate/onetomany/model/Comment.java
+++ src/main/java/com/bezkoder/spring/hibernate/onetomany/model/Comment.java
@@ -3,6 +3,7 @@ package com.bezkoder.spring.hibernate.onetomany.model;
 import jakarta.persistence.*;
 import org.hibernate.annotations.OnDelete;
 import org.hibernate.annotations.OnDeleteAction;
+import javax.validation.constraints.NotBlank; // Importar la anotación @NotBlank
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
@@ -13,6 +14,7 @@ public class Comment {
   private Long id;
 
   @Lob
+  @Column(nullable = false) // Agregar la anotación @Column con nullable=false para evitar que el contenido sea nulo a nivel de base de datos.
   private String content;
 
 //  @ManyToOne(fetch = FetchType.EAGER, optional = false)
